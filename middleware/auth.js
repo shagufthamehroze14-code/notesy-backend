3f5d1b23c62a73e4e613d0dcaf14d8bce9247061
shagufthamehroze14-code <shagufthamehroze14@gmail.com>
2026-01-13 04:31:00 +0300

    Fixed case sensitivity bug

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 
 // Protect routes - verify token
 exports.protect = async (req, res, next) => {
